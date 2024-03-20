50c9cd6d5562485172fde2e3de0500b0da38fc5f
zozero <zoneonull@gmail.com>
2024-03-20 20:32:08 +0800

    添加流程控制； 添加星号功能判定。

--- 主入口.py
+++ 主入口.py
@@ -1,20 +1,33 @@
 import pandas as pd
+
 # 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
+    当前索引 = 0
+    计数 = 100
+    while True:
+        列表 = [
+            {
+                "数据": "数据1",
+                "前往": 1
+            },
+            {
+                "数据": "数据2",
+                "前往": 2
+            },
+            {
+                "数据": "数据3",
+                "前往": 3
+            },
+            {
+                "数据": "数据4",
+                "前往": 1
+            }
+        ]
 
-    import re
-
-    # 假设我们要匹配一个点（.）
-    text = "This is a sentence This is another sentence"
-
-    # 正则表达式中点（.）需要转义
-    escaped_dot = "\."
-
-    # 使用re模块的search方法进行匹配
-    matches = re.search(escaped_dot, text)
-
-    # 如果匹配成功，打印匹配结果
-    if matches:
-        print("匹配成功:", matches.group())
-    else:
-        print("没有匹配到点（.）")
+        print(列表[当前索引]["数据"])
+        当前索引 = 列表[当前索引]["前往"]
+        if 当前索引 == -1:
+            break
+        计数 -= 1
+        if 计数 == 0:
+            break
