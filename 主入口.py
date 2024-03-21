3e8ad657b75feca13dbcd0999655b0b1ee5a8fe3
zozero <zoneonull@gmail.com>
2024-03-21 16:54:28 +0800

    优化设置序号

--- 主入口.py
+++ 主入口.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 # 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-
-    if "":
-        print(1111)
+    df = pd.DataFrame({'name': ['Raphael', 'Donatello', 'Donatello2', 'Donatello3'],
+                       'mask': ['red', 'purple', 'purple2', 'purple3'],
+                       'weapon': ['sai', 'bo staff', 'bo staff2', 'bo staff3']},index=['name'])
+    print(df)
+    print(df[-1:].index)
--- 通用对象屋/表格对象.py
+++ 通用对象屋/表格对象.py
@@ -26,12 +26,14 @@ class 表格处理类:
     """
     主要是数据的增加或者减少
     创建对应的表格
+    确保每个表格都会有序号
     """
 
     def __init__(self, 完整路径, 一条数据: dict):
         self.完整路径 = 完整路径
         self.一条数据 = 一条数据
         self.数据数量 = 0
+        self.序号尾巴 = 0
         self.数据表 = None
         self.读取数据表()
 
@@ -43,10 +45,12 @@ class 表格处理类:
             self.创建csv表格()
 
         self.数据数量 = len(self.数据表)
+        if self.数据数量 > 0:
+            self.序号尾巴 = self.数据表[-1:].index[0]
 
     def 添加数据(self):
         if self.一条数据["序号"][0] is None or self.一条数据["序号"][0] == "":
-            self.一条数据["序号"][0] = self.数据数量 + 1
+            self.一条数据["序号"][0] = self.序号尾巴 + 1
         数据帧 = pd.DataFrame(self.一条数据)
         数据帧.to_csv(self.完整路径, mode='a', header=False, index=False, encoding=统一编码)
 
